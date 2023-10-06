6f83645bfc44395586ccb86a809e2ccfce886e14
Imariiii <anna.ostanina0108@mail.ru>
2023-10-06 23:17:21 +0300

    Deleted files

--- build/CTestTestfile.cmake
+++
@@ -1,8 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/anna/os_labs
-# Build directory: /home/anna/os_labs/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("lab1")
-subdirs("tests")
