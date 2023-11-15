cc497fad8f42a7367cf07034891fd3176bec66ff
Imariiii <anna.ostanina0108@mail.ru>
2023-11-15 17:57:36 +0300

    Add header

---
+++ lab2/include/sort.h
@@ -0,0 +1,22 @@
+#pragma once
+#include <vector>
+
+class ParallelSort {
+    using ll = long long;
+
+   public:
+    static void QuickSort(std::vector<ll>& vec, unsigned int threadsNum);
+
+   private:
+    struct QuickSortArgs {
+        std::vector<ll>* vec;
+        std::size_t left;
+        std::size_t right;
+    };
+
+    static void* QuickSort(void* ptr);
+    static void QuickSort(std::vector<ll>& vec, std::size_t left,
+                          std::size_t right);
+    static std::size_t Partition(std::vector<ll>& vec, std::size_t left,
+                                 std::size_t right);
+};
